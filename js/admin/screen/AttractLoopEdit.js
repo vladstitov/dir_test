9f9799a922e2f41c768dcfc507108c4ec8fea1c1
vladvaldtitov <vlad.vlad.titov@gmail.com>
2015-08-11 21:51:45 -0400

    AttractLoop started

---
+++ js/admin/screen/AttractLoopEdit.js
@@ -0,0 +1,14 @@
+/**
+ * Created by VladHome on 8/11/2015.
+ */
+/// <reference path="../RegA.ts" />
+var uplight;
+(function (uplight) {
+    var AttractLoopEdit = (function () {
+        function AttractLoopEdit(container) {
+        }
+        return AttractLoopEdit;
+    })();
+    uplight.AttractLoopEdit = AttractLoopEdit;
+})(uplight || (uplight = {}));
+//# sourceMappingURL=AttractLoopEdit.js.map
---
+++ js/admin/screen/AttractLoopEdit.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by VladHome on 8/11/2015.
+ */
+    /// <reference path="../RegA.ts" />
+module uplight{
+    export class AttractLoopEdit{
+        constructor(container:JQuery){
+
+        }
+
+}
+}
